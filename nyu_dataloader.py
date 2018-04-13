9910bd624507c1b76dd5817876e2157aad7b6286
Lishunkai <worldprc@qq.com>
2018-04-13 10:50:58 +0800

    update

--- nyu_dataloader.py
+++ nyu_dataloader.py
@@ -156,19 +156,19 @@ class NYUDataset(data.Dataset):
         sparse_depth[mask_keep] = depth[mask_keep] # 把深度图中和mask_keep对应的元素赋值给sparse_depth中的对应元素
         return sparse_depth
 
-	# 生成稀疏深度图 ORB特征提取
-    def create_sparse_depth_ORB(self, rgb, depth, prob):
-        num_samples = int(prob * depth.size)
-        gray = rgb2grayscale(rgb)
-        orb = cv2.ORB_create(num_samples)
-        kp = orb.detectAndCompute(gray, None)
-        print len(kp)
-        # img2 = cv2.drawKeypoints(gray, kp, (255,0,0), 1)
+	# # 生成稀疏深度图 ORB特征提取
+    # def create_sparse_depth_ORB(self, rgb, depth, prob):
+    #     num_samples = int(prob * depth.size)
+    #     gray = rgb2grayscale(rgb)
+    #     orb = cv2.ORB_create(num_samples)
+    #     kp = orb.detect(gray, None)
+    #     print len(kp)
+    #     # img2 = cv2.drawKeypoints(gray, kp, (255,0,0), 1)
 
-        mask_keep = np.random.uniform(0, 1, depth.shape) < prob # 生成一个0-1的mask，0-1是随机产生的，0-1产生的概率小于预设的概率
-        sparse_depth = np.zeros(depth.shape)
-        sparse_depth[mask_keep] = depth[mask_keep] # 把深度图中和mask_keep对应的元素赋值给sparse_depth中的对应元素
-        return sparse_depth
+    #     mask_keep = np.random.uniform(0, 1, depth.shape) < prob # 生成一个0-1的mask，0-1是随机产生的，0-1产生的概率小于预设的概率
+    #     sparse_depth = np.zeros(depth.shape)
+    #     sparse_depth[mask_keep] = depth[mask_keep] # 把深度图中和mask_keep对应的元素赋值给sparse_depth中的对应元素
+    #     return sparse_depth
 
 
 
