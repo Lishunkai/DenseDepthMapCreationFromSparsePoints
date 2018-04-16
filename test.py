10b111e56ba3cd447c1de3c1af1d04acfb167991
Lishunkai <worldprc@qq.com>
2018-04-16 14:39:44 +0800

    update

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
 import cv2
+import numpy as np
 
 rgb = cv2.imread('1.png')
 gray = cv2.cvtColor(rgb,cv2.COLOR_BGR2GRAY)
 #gray = rgb[:,:,0] * 0.2989 + rgb[:,:,1] * 0.587 + rgb[:,:,2] * 0.114
 orb = cv2.ORB_create(500)
 kp = orb.detect(gray, None)
-img2 = cv2.drawKeypoints(gray,kp,(225,0,0),-1)
+print(len(kp))
 
-cv2.namedWindow("rgb")
-cv2.imshow("rgb", img2)
-cv2.namedWindow("gray")
-cv2.imshow("gray",gray)
-cv2.waitKey(0)
+sparse_depth = np.zeros(rgb.shape)
+
+for i in range(len(kp)):
+    tu = kp[i].pt
+    x = int(tu[0])
+    y = int(tu[1])
+    print(i,'  ',x,'  ',y)
+    sparse_depth[y,x] = rgb[y,x] # x,y和行,列的顺序不一样
+
+
+# print(len(kp)) # keypoint的个数
+# print(kp[0]) # 多少个角点，就有多少个下标
+# tu = kp[0].pt   #（提取坐标） pt指的是元组 tuple(x,y)
+# print(tu[0],tu[1]) # 输出第一个keypoint的x,y坐标 
+
 
     # def create_sparse_depth_ORB(self, rgb, depth, prob):
     #     num_samples = int(prob * depth.size)
@@ -19,7 +30,6 @@ cv2.waitKey(0)
     #     orb = cv2.ORB_create(num_samples)
     #     kp = orb.detect(gray, None)
     #     print len(kp)
-    #     # img2 = cv2.drawKeypoints(gray, kp, (255,0,0), 1)
 
     #     mask_keep = np.random.uniform(0, 1, depth.shape) < prob # 生成一个0-1的mask，0-1是随机产生的，0-1产生的概率小于预设的概率
     #     sparse_depth = np.zeros(depth.shape)
